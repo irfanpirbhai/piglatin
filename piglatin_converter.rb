0afae805db5868d4d4a6914c71c92c91f4fb1ec2
Irfan Pirbhai <irfan.pirbhai@gmail.com>
2013-03-16 15:04:26 -0400

    refactored,deleted comments

--- piglatin_converter.rb
+++ piglatin_converter.rb
@@ -1,26 +1,3 @@
-# class PiglatinConverter
-#   def self.convert(a)
-#   	if a == ""
-#    		return ""
-# 	elsif a == nil
-# 		return nil
-# 	elsif a != a[/[a-zA-Z]+/]
-#   		return nil
-# 	else
-#   		a = a.downcase
-#   		ind_val = a.index(/[aeiou]/)
-#   			if ind_val == 0
-#   				return a+"way"
-# 			else
-#   				leftside = a[0...ind_val]
-#   				rightside = a[ind_val..-1]
-# 				return rightside + leftside + "ay"
-# 			end
-# 	end
-#   end
-# end
-
-# REFACTORED!
 class PiglatinConverter
   def self.convert(a)
   	return a if (a == "") or (a == nil)
